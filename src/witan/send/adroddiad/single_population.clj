7eee03cbd808865956f8302ebffbbb2ac54346a6
Seb Bailey <sebastian@mastodonc.com>
2022-02-19 21:08:31 +0000

    Compare total populations

--- src/witan/send/adroddiad/single_population.clj
+++ src/witan/send/adroddiad/single_population.clj
@@ -3,7 +3,8 @@
             [kixi.plot :as plot]
             [kixi.plot.colors :as colors]
             [kixi.plot.series :as series]
-            [witan.send.adroddiad.chart-utils :as chart-utils]))
+            [witan.send.adroddiad.chart-utils :as chart-utils]
+            [tablecloth.api :as tc]))
 
 (defn single-population-report [{:keys [census-data color shape legend-label title watermark base-chart-spec chartf]
                                  :or {base-chart-spec plot/base-pop-chart-spec
@@ -14,6 +15,26 @@
               ::plot/title {::plot/label title}}
              base-chart-spec
              {::large/data census-data
+              ::large/sheet-name title})
+      (plot/add-overview-legend-items)
+      (chartf)
+      (update ::plot/canvas plot/add-watermark watermark)
+      (chart-utils/->large-charts)))
+
+(defn two-populations-report [{:keys [first-census second-census colors shapes legend-label title watermark base-chart-spec chartf]
+                               :or {base-chart-spec plot/base-pop-chart-spec
+                                    chartf plot/zero-y-index}}]
+  (-> {::series/series (into []
+                             (mapcat (fn [[ds col]] (series/ds->median-iqr-95-series ds col (nth shapes 0))))
+                             [[first-census (nth colors 0)] [second-census (nth colors 1)]])
+       ::series/legend-spec [(series/legend-spec legend-label (nth colors 0) (colors/legend-shape (nth shapes 0)))]
+       #_(into []
+               (mapcat (fn [[ds col s]] (series/legend-spec ds col (colors/legend-shape s))))
+               [["Scenario Population" (nth colors 0) (nth shapes 0)] ["Baseline Population" (nth colors 1) (nth shapes 1)]])}
+      (merge {::plot/legend-label legend-label
+              ::plot/title {::plot/label title}}
+             base-chart-spec
+             {::large/data (tc/append first-census second-census)
               ::large/sheet-name title})
       (plot/add-overview-legend-items)
       (chartf)
