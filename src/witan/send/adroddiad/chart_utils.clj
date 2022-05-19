d54794f220743b49994330c3924f1b463891c85b
Seb Bailey <seb231@me.com>
2022-05-19 16:07:10 +0100

    Fix two arity

--- src/witan/send/adroddiad/chart_utils.clj
+++ src/witan/send/adroddiad/chart_utils.clj
@@ -19,7 +19,7 @@
           (conj (vals (domains census-data))
                 extra-domain-items)) palette))
   ([census-data extra-domain-items]
-   (colors-and-shapes extra-domain-items))
+   (colors-and-shapes census-data extra-domain-items))
   ([census-data]
    (colors-and-shapes census-data [])))
 
