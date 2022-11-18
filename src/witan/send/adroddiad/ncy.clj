375acb8899a673d4d5dd222c967a710f9560da92
eraderna <andrew.holmes@mastodonc.com>
2022-11-18 20:47:53 +0000

    WiP on updated ncy namespace
    - update ncy.clj to:
      - replace age->ncy & ncy->age (with offset of 5) with age-at-start-of-school-year->ncy & ncy->age-at-start-of-school-year (with offset of 4) as the latter aligns with definition of age for SEN2 groupings and DSG Management Plan template.
      - add draft of function impute-nil-ncy
      - add WiP on testing: notes on what to test and basic (rcf/tests)
    - update deps.edn to add com.hyperfiddle/rcf for testing
    - add user.clj to enable hyperfiddle.rcf

--- src/witan/send/adroddiad/ncy.clj
+++ src/witan/send/adroddiad/ncy.clj
@@ -1,7 +1,99 @@
-(ns witan.send.adroddiad.ncy)
+(ns witan.send.adroddiad.ncy
+  (:require [tablecloth.api :as tc]
+            [hyperfiddle.rcf :as rcf]))
 
-(defn ncy->age [ncy]
-  (+ ncy 5))
 
-(defn age->ncy [age]
-  (- age 5))
+
+(defn age-at-start-of-school-year->ncy [age]
+  (- age 4))
+
+
+
+(defn ncy->age-at-start-of-school-year [ncy]
+  (+ ncy 4))
+
+
+
+(defn impute-nil-ncy
+  "Impute values for National Curriculum Year (NCY) where nil in `ds`.
+
+   Imputation is performed for pupils with at least one record with a
+   non-nil NCY and non-nil census/calendar year, using the first such
+   pair (with smallest NCY) as a reference point from which to calculate
+   the nil NCYs, assuming 1 NCY per census/calendar-year. For rows where
+   imputation is possible the nil NCY is replaced with the imputed value
+   and a string describing the imputation mapped into a separate
+   column. For rows where imputation is not possible NCY is left nil.
+
+   Columns of `ds` to use are specified in (optional) map as follows:
+   `id`             - unique identifier for the pupil {default :id}
+   `cy`             - calendar|census year of the row {default :calendar-year}
+   `ncy`            - National Curriculum Year of the row {default :academic-year}
+   `ncy-imputation` - imputation description column in returned dataset {default :academic-year-imputation}"
+  ([ds]
+   (impute-nil-ncy ds {}))
+  ([ds {:keys [id cy ncy ncy-imputed ncy-imputation]
+        :or   {id             :id
+               cy             :calendar-year
+               ncy            :academic-year
+               ncy-imputed    :academic-year
+               ncy-imputation :academic-year-imputation}}]
+   (-> ds
+       (tc/left-join (-> ds
+                         (tc/select-columns [id ncy cy])
+                         (tc/drop-missing)
+                         (tc/order-by       [id ncy cy])
+                         (tc/group-by       [id       ])
+                         (tc/first)
+                         (tc/ungroup)
+                         (tc/rename-columns {ncy :ncy-ref
+                                             cy  :ref-cy})
+                         (tc/set-dataset-name "_ref"))
+                     [id])
+       (tc/map-columns ncy-imputation [ncy :ncy-ref cy :ref-cy ]
+                       (fn [ncy ncy-ref cy cy-ref]
+                         (when (and (nil? ncy) (some? ncy-ref) (some? cy) (some? cy-ref))
+                           (str "Imputed from NCY=" ncy-ref " in " cy-ref ))))
+       (tc/map-columns ncy-imputed    [ncy :ncy-ref cy :ref-cy ncy-imputation]
+                       (fn [ncy ncy-ref cy cy-ref ncy-imputation]
+                         (if (and (nil? ncy) (some? ncy-imputation))
+                           (- ncy-ref (- cy-ref cy))
+                           ncy)))
+       (tc/drop-columns #(= "_ref" %) :src-table-name))))
+
+(comment
+  ;; Checks & tests:
+  ;; - default column names
+  ;; - imputation cases
+  ;;   - no imputation needed
+  ;;   - single record nil NCY
+  ;;   - multiple records nil NCY
+  ;;   - single record nil NCY, single ref record
+  ;;     - CY for nil NCY < that of ref record
+  ;;     - CY for nil NCY > that of ref record
+  ;;     - CY for nil NCY cf. ref record such that NCY imputes <0
+  ;;     - CY for nil NCY cf. ref record such that NCY imputes >20
+  ;;   - multiple record nil NCY, multiple ref record
+  ;;     - nil NCY interlaced with ref records
+  ;; - existing ncy-imputation column with non-nil values?
+  ;; - different column names
+
+  )
+
+
+
+(comment
+
+  )
+
+
+
+(rcf/tests
+
+ "Age at start of reception should be 4, per SEN2 guidelines"
+ (ncy->age-at-start-of-school-year 0) := 4
+
+ [1 2 3] := (take 3 [1 2 3 4])
+
+ )
+
