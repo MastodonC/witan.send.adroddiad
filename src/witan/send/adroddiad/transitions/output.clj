4eac5f73e89b151acdd9a5123c7b59bf11cea41c
eraderna <andrew.holmes@mastodonc.com>
2023-02-07 17:56:18 +0000

    Allow additional columns in output CSVs
    - Factor core census/transition column names into externally accessible def
    - Add optional to ->csv-with-ids to include additional columns (ensuring that the core columns come first in the expected order).

--- src/witan/send/adroddiad/transitions/output.clj
+++ src/witan/send/adroddiad/transitions/output.clj
@@ -2,19 +2,22 @@
   (:require [tablecloth.api :as tc]
             [witan.send.adroddiad.transitions :as t]))
 
-(defn ->csv-with-ids [transitions out-file]
-  (-> transitions
-      (tc/select-columns [:id :calendar-year
-                          :setting-1 :need-1 :academic-year-1
-                          :setting-2 :need-2 :academic-year-2
-                          :transition-type])
-      (tc/order-by [:id :calendar-year])
-      (tc/map-columns :transition-type [:setting-1 :setting-2] t/transition-type)
-      (tc/write! out-file)))
+(def transition-column-names
+  [:calendar-year
+   :setting-1 :need-1 :academic-year-1
+   :setting-2 :need-2 :academic-year-2])
+
+(defn ->csv-with-ids
+  ([transitions out-file]
+   (->csv-with-ids transitions out-file []))
+  ([transitions out-file additional-column-names]
+   (-> transitions
+       (tc/map-columns :transition-type [:setting-1 :setting-2] t/transition-type)
+       (tc/select-columns ((comp distinct concat) [:id] transition-column-names [:transition-type] additional-column-names))
+       (tc/order-by [:id :calendar-year])
+       (tc/write! out-file))))
 
 (defn ->csv-model-ready [transitions out-file]
   (-> transitions
-      (tc/select-columns [:calendar-year
-                          :setting-1 :need-1 :academic-year-1
-                          :setting-2 :need-2 :academic-year-2])
+      (tc/select-columns transition-column-names)
       (tc/write! out-file)))
