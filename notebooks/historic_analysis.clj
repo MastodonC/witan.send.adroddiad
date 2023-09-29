66cafa41e4e75c10ecac10bcd8ea4d7d0b86aa49
Seb Bailey <seb231@me.com>
2023-09-29 14:50:45 +0100

    Validation title

--- notebooks/historic_analysis.clj
+++ notebooks/historic_analysis.clj
@@ -4,12 +4,17 @@
    :nextjournal.clerk/visibility           {:code   :hide
                                             :result :show}
    :nextjournal.clerk/open-graph
-   {:title "Hello World!" #_(format "MC:%s %s %s %s" LA date work-package title)
-    :image "https://www.mastodonc.com/wp-content/themes/MastodonC-2018/dist/images/favicon.png"}}
+   {:image "https://www.mastodonc.com/wp-content/themes/MastodonC-2018/dist/images/favicon.png"}}
   (:require [nextjournal.clerk :as clerk]
             [nextjournal.clerk-slideshow :as slideshow]
             [tablecloth.api :as tc]))
 
+(def la nil)
+
+(def year nil)
+
+(def date nil)
+
 ^{:nextjournal.clerk/visibility {:result :show}}
 (clerk/md (str "![Mastodon C](https://www.mastodonc.com/wp-content/themes/MastodonC-2018/dist/images/logo_mastodonc.png)  \n"
-               "# Hounslow SEND Setting Processing README 2023-10-05"))
+               "# " la " SEND " year " Validation " date))
