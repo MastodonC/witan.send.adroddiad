aa9bceff4906adb0ac6e698ccec7a35f9273349d
Seb Bailey <seb231@me.com>
2023-10-03 14:39:21 +0100

    Template inputs

--- notebooks/historic_analysis.clj
+++ notebooks/historic_analysis.clj
@@ -15,6 +15,22 @@
 (def date-string nil)
 (def out-dir nil)
 (def workpackage-name nil)
+(def in-dir nil)
+(def census (tc/dataset (str in-dir nil) (:key-fn keyword))) ;; consider defaulting to filename
+(def transitions (tc/dataset (str in-dir nil) {:key-fn keyword})) ;; consider defaulting to filename
+
+(
+ ;; Supporting functions and defs
+ )
+
+(clerk/add-viewers! [slideshow/viewer])
+
+(comment
+  ;; Writes out to to a standalone html file
+  (chtml/ns->html out-dir *ns*)
+
+  )
+
 
 {:nextjournal.clerk/visibility {:result :show}}
 (clerk/md (str "![Mastodon C](https://www.mastodonc.com/wp-content/themes/MastodonC-2018/dist/images/logo_mastodonc.png)  \n"
