249fb960ae1875cf083fddf8ef5fc1327326eafd
Seb Bailey <seb231@me.com>
2024-01-12 15:34:16 +0000

    Example vega-lite chart map for testing

--- src/witan/send/adroddiad/transcode.clj
+++ src/witan/send/adroddiad/transcode.clj
@@ -43,3 +43,20 @@
           out (TranscoderOutput. out-stream)
           trans (high-quality-png-transcoder)]
       (.transcode trans in out))))
+(comment
+
+  (def example-vega-lite-chart-map
+    {:data {:values [{:a "A" :b 28}
+                     {:a "B" :b 55}
+                     {:a "C" :b 43}
+                     {:a "D" :b 91}
+                     {:a "E" :b 81}
+                     {:a "F" :b 53}
+                     {:a "G" :b 19}
+                     {:a "H" :b 87}
+                     {:a "I" :b 52}]}
+     :encoding {:x {:axis {:labelAngle 0} :field "a" :type "nominal"}
+                :y {:field "b" :type "quantitative"}}
+     :mark "bar"})
+
+  )
