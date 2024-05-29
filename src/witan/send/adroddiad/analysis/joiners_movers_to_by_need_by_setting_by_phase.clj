a8ac3ed16c3e9113e4e6c286f2966be48ec3e22e
Bruce Durling <bruce@mastodonc.com>
2024-05-29 19:10:29 +0100

    WIP

--- src/witan/send/adroddiad/analysis/joiners_movers_to_by_need_by_setting_by_phase.clj
+++ src/witan/send/adroddiad/analysis/joiners_movers_to_by_need_by_setting_by_phase.clj
@@ -50,3 +50,33 @@
                            (tc/order-by numerator-grouping-keys)
                            )))
                    :simulation-count (get-in cfg [:projection-parameters :simulations])})))
+
+(def base-chart-spec
+  {:y   :median
+   :irl :q1  :iru :q3  :ir-title "50% range"
+   :orl :p05 :oru :p95 :or-title "90% range"})
+
+(def half-width 700)
+(def third-width 475)
+
+(defn chart [need-by-setting-by-phase-summaries
+             {:keys [setting setting-label phase need-colors-and-shapes y-scale y-zero]
+              :or   {y-scale false
+                     y-zero  true}}]
+  (let [label-field :need
+        data        (-> (get-in need-by-setting-by-phase-summaries [:total-summary :table])
+                        (tc/select-rows #(and (= phase (:phase %))
+                                              (= setting (:setting %)))))]
+    (vsl/line-and-ribbon-and-rule-plot
+     (merge base-chart-spec
+            {:data              (-> data
+                                    (tc/map-columns :calendar-year [:calendar-year] td/format-calendar-year))
+             :chart-title       (format "# EHCP by %s for %s %s" (name label-field) setting-label phase)
+             :chart-height      vs/full-height :chart-width vs/two-thirds-width
+             :tooltip-formatf   (vsl/number-summary-tooltip {:group label-field :x :calendar-year :tooltip-field :tooltip-column})
+             :colors-and-shapes need-colors-and-shapes
+             :x                 :calendar-year :x-title     "Census Year at January" :x-format "%b %Y"
+             :y-title           "# EHCPs"      :y-zero      y-zero                   :y-scale  y-scale
+             :group             label-field    :group-title (clojure.string/capitalize (name label-field))     
+             ;; :order-field order-field
+             }))))
