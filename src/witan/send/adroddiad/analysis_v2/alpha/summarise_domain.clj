607f67666116a15376554fa41b9a48d1b95f9e51
eraderna <andrew.holmes@mastodonc.com>
2025-01-08 18:18:37 +0000

    Simpler replacement for add-diff for sparse input

--- src/witan/send/adroddiad/analysis_v2/alpha/summarise_domain.clj
+++ src/witan/send/adroddiad/analysis_v2/alpha/summarise_domain.clj
@@ -255,3 +255,85 @@
         (tc/reorder-columns (concat key-cols [value-col previous-value-col diff-col]))
         (tc/order-by key-cols))))
 
+;; Replacement for add-diff to cope with sparse input:
+;; See test ns `witan.send.adroddiad.analysis-v2.alpha.summarise-domain-test` for rationale and examples.
+(defn add-previous-year-diff-to-sparse
+  "Given dataset `ds` containing (potentially sparse) time-series at (integer) `:calendar-year`s
+   of values (in column `value-col`), returns a dataset with the previous years value and
+   differences added.
+
+   To permit use within groups, if `group-cols` are specified then corresponding
+   columns are also included in the returned dataset with values taken from the
+   first row of `ds`.
+
+   Sparsity
+   The input dataset `ds` may be sparse, i.e. omit `:calendar-year`s where the value is 0.
+   In this case:
+   - The range [`min-cy`, `max-cy`] (maths interval notation) of possible `:calendar-year`s,
+     unless specified, is taken from the input dataset.
+   - The value at a `:calendar-year` that is within range but not in `ds` is assumed to be 0.
+   - The value at a `:calendar-year` outside the specified range is assumed unknown (`nil`),
+     even if a value was provided in the input dataset `ds`.
+   - The output dataset will include a record for all `:calendar-year`s in the range.
+     (Thus, given sparse input, the output dataset will have additional rows.)
+
+  Trailing options map/kv-pairs keys are as follows:
+   - `value-col` (default `:transition-count`) - name of value column.
+   - `min-cy` & `max-cy` (optional) - range of `:calendar-year`s to consider
+     (each calculated from the dataset if not provided).
+   - `previous-value-col` (optional) - name for previous value column in the returned dataset
+     (defaults to names of the value column with '-previous' appended).
+   - `diff-col` (optional) - name for diff column in the returned dataset
+     (defaults to names of the value column with '-diff' appended).
+   - `group-cols` (default `nil`) - column-selector for grouping columns.
+
+  Note:
+  - Due to handling sparsity, no other columns of the input dataset `ds` are returned.
+  - The `:calendar-year` is used as a map key and therefore must be unique.
+  - Any `:calendar-year`s outside the specified range are ignored.
+  - The previous value and diff for the `min-cy` `:calendar-year` are returned as `nil`.
+  "
+  [ds & {:keys [value-col min-cy max-cy previous-value-col diff-col group-cols]
+         :or   {value-col :count}}]
+  (let [min-cy             (or min-cy (-> ds :calendar-year tcc/reduce-min))
+        max-cy             (or max-cy (-> ds :calendar-year tcc/reduce-max))
+        previous-value-col (or previous-value-col (-> value-col name (str "-previous") keyword))
+        diff-col           (or diff-col           (-> value-col name (str "-diff")     keyword))
+        cy->value          (zipmap (ds :calendar-year) (ds value-col))
+        group-cols-row     (-> ds (tc/select-columns group-cols) (tc/rows :as-maps) first)]
+    (when (< (count cy->value) (tc/row-count ds))
+      (throw (ex-info (str "Some `:calendar-year`s repeated in input dataset.")
+                      {:ds ds, :calendar-years (->> ds :calendar-year frequencies (filter #(-> % second (> 1))) keys)})))
+    (-> (tc/dataset {:calendar-year (range min-cy (inc max-cy))})
+        (tc/map-columns value-col [:calendar-year] #(get cy->value % 0))
+        (tc/add-column previous-value-col #(into [nil] (-> % value-col butlast)))
+        (tc/map-columns diff-col [value-col previous-value-col] #(when %2 (- %1 %2)))
+        (tc/map-rows (constantly group-cols-row))
+        (tc/reorder-columns group-cols))))
+
+(defn add-previous-year-diff-to-sparse-by-group
+  "A wrapper for `add-previous-year-diff` for grouped data:
+   Groups `ds` by the `group-cols`, calls `add-previous-year-diff` for each and ungroups.
+   Trailing argument options map/kv-pairs are as for `add-previous-year-diff`,
+   but are parsed here for defaults to avoid repetition and so that default
+   `min-cy` & `max-cy` are calculated over all groups."
+  [ds & {:keys [value-col min-cy max-cy previous-value-col diff-col group-cols]}]
+  (let [value-col          (or value-col :count)
+        min-cy             (or min-cy (-> ds :calendar-year tcc/reduce-min))
+        max-cy             (or max-cy (-> ds :calendar-year tcc/reduce-max))
+        previous-value-col (or previous-value-col
+                               (-> value-col name (str "-previous") keyword))
+        diff-col           (or diff-col
+                               (-> value-col name (str "-diff") keyword))
+        group-cols         (tc/column-names ds group-cols)]
+    (-> ds
+        (tc/group-by group-cols)
+        (tc/process-group-data
+         #(add-previous-year-diff-to-sparse % {:value-col          value-col
+                                               :min-cy             min-cy
+                                               :max-cy             max-cy
+                                               :previous-value-col previous-value-col
+                                               :diff-col           diff-col
+                                               :group-cols         group-cols}))
+        tc/ungroup)))
+
