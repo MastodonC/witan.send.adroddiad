010f851a7f096d890dc1cd15790752655b872110
eraderna <andrew.holmes@mastodonc.com>
2025-06-02 16:41:07 +0100

    Update ncy->school-phase in line with key-stage

--- src/witan/send/adroddiad/school_phase.clj
+++ src/witan/send/adroddiad/school_phase.clj
@@ -18,10 +18,12 @@
 
 
 ;;; # Utility functions
-(defn compare-mapped-keys
-  [m k1 k2]
-  (compare [(get m k1) k1]
-           [(get m k2) k2]))
+(defn key-comparator-fn-by
+  [key->order]
+  (fn [k1 k2]
+    (compare [(key->order k1) k1]
+             [(key->order k2) k2])))
+
 
 
 ;;; # Mastodon C School Phases
@@ -39,38 +41,38 @@
 
 (def school-phases
   "Mastodon C School Phase definitions as a sorted map."
-  (as-> {"early-childhood" {:order      0
-                            :name       "Early Childhood Education and Care"
-                            :label      "Early Childhood"
-                            :definition "Prior to starting school: Age 0-4"
-                            :ncy-from   -4 ;; Note witan.send.domain.academic-years/nursery includes -5
-                            :ncy-to     -1}
-         "primary"         {:order      1
-                            :name       "Primary"
-                            :label      "Primary"
-                            :definition "Primary school: Reception, KS1 & KS2 - Reception + NCYs 1 to 6"
-                            :ncy-from   0
-                            :ncy-to     6}
-         "secondary"       {:order      2
-                            :name       "Secondary"
-                            :label      "Secondary"
-                            :definition "Secondary school: Key Stages 3 + 4 - NCYs 7 to 11"
-                            :ncy-from   7
-                            :ncy-to     11}
-         "post-16"         {:order      3
-                            :name       "Post 16"
-                            :label      "Post 16"
-                            :definition "Post 16 - Key Stage 5 - NCYs 12 to 14"
-                            :ncy-from   12
-                            :ncy-to     14}
-         "post-19"         {:order      4
-                            :name       "Post 19"
-                            :label      "Post 19"
-                            :definition "Post 19 - Post Key Stage 5 up to 25 years of age - NCYs 15 to 20"
-                            :ncy-from   15
-                            :ncy-to     20}} $
-    (update-vals $ (fn [m] (assoc m :ncys (into (sorted-set) (range (:ncy-from m) (inc (:ncy-to m)))))))
-    (into (sorted-map-by (partial compare-mapped-keys (update-vals $ :order))) $)))
+  (-> {"early-childhood" {:order      0
+                          :name       "Early Childhood Education and Care"
+                          :label      "Early Childhood"
+                          :definition "Prior to starting school: Age 0-4"
+                          :ncy-from   -4 ;; Note witan.send.domain.academic-years/nursery includes -5
+                          :ncy-to     -1}
+       "primary"         {:order      1
+                          :name       "Primary"
+                          :label      "Primary"
+                          :definition "Primary school: Reception, KS1 & KS2 - Reception + NCYs 1 to 6"
+                          :ncy-from   0
+                          :ncy-to     6}
+       "secondary"       {:order      2
+                          :name       "Secondary"
+                          :label      "Secondary"
+                          :definition "Secondary school: Key Stages 3 + 4 - NCYs 7 to 11"
+                          :ncy-from   7
+                          :ncy-to     11}
+       "post-16"         {:order      3
+                          :name       "Post 16"
+                          :label      "Post 16"
+                          :definition "Post 16 - Key Stage 5 - NCYs 12 to 14"
+                          :ncy-from   12
+                          :ncy-to     14}
+       "post-19"         {:order      4
+                          :name       "Post 19"
+                          :label      "Post 19"
+                          :definition "Post 19 - Post Key Stage 5 up to 25 years of age - NCYs 15 to 20"
+                          :ncy-from   15
+                          :ncy-to     20}}
+      (update-vals (fn [m] (assoc m :ncys (into (sorted-set) (range (:ncy-from m) (inc (:ncy-to m)))))))
+      (as-> $ (into (sorted-map-by (key-comparator-fn-by (update-vals $ :order))) $))))
 
 (def school-phases-ds
   "Mastodon C School Phase definitions as a dataset."
@@ -83,15 +85,24 @@
 
 
 ;;; # Functions to manipulate school-phases
+(defn ncy->school-phase-map
+  "Given [optional] `school-phases` definition map (defaults to namespace definition) 
+   mapping each school-phase to a map containing a `:ncys` key whose value is a 
+   collection of the NCYs for that school phase, returns a map mapping NCY to school-phase.
+   Note that if an NCY is (erronously) specified in multiple school-phases then the
+   first defined is used."
+  ([] (ncy->school-phase-map school-phases))
+  ([school-phases]
+   (into (sorted-map)
+         (map (fn [[k {:keys [ncys]}]]
+                (zipmap ncys (repeat k))))
+         (reverse school-phases))))
+
 (defn ncy->school-phase
-  "Given National Curriculum Year `x` and [optional] map of `school-phases`,
-   returns the abbreviation for the MC School Phase containing it.
-  `school-phases` must be a map with keys the school phase
-   and values maps containing a `:ncys` key whose value is a collection of the NCYs for that school phase.
-   Defaults to the namespace `school-phases` if not specified."
+  "Given National Curriculum Year `x` and [optional] `school-phases` definition map
+   mapping each school-phase to a map containing a `:ncys` key whose value is a 
+   collection of the NCYs for that school phase, returns the school-phase (key) for
+   containing it. `school-phases` defaults fo the namespace definition if not specified."
   ([x] (ncy->school-phase x school-phases))
   ([x school-phases]
-   (some (fn [[k {:keys [ncys]}]]
-           (when (contains? ncys x) k))
-         school-phases)))
-
+   (get (ncy->school-phase-map school-phases) x)))
--- test/witan/send/adroddiad/school_phase_test.clj
+++ test/witan/send/adroddiad/school_phase_test.clj
@@ -2,6 +2,77 @@
   (:require [clojure.test :refer [deftest testing is]]
             [witan.send.adroddiad.school-phase :as school-phase]))
 
+(deftest ncy->school-phase-map
+  (testing "Default mapping of NCY to key stage."
+    (is (= (school-phase/ncy->school-phase-map)
+           (merge (zipmap [-4 -3 -2 -1]       (repeat "early-childhood"))
+                  (zipmap [0 1 2 3 4 5 6]     (repeat "primary"))
+                  (zipmap [7 8 9 10 11]       (repeat "secondary"))
+                  (zipmap [12 13 14]          (repeat "post-16"))
+                  (zipmap [15 16 17 18 19 20] (repeat "post-19"))))))
+
+  (testing "Custom `school-phases` definitions, with `:ncys` as sets."
+    (is (= (-> {"Phase A" {:ncys #{109 108}}
+                "Phase B" {:ncys #{110 111 112}}}
+               school-phase/ncy->school-phase-map)
+           ;; Note: Map is sorted by NCY
+           (sorted-map 108 "Phase A"
+                       109 "Phase A"
+                       110 "Phase B"
+                       111 "Phase B"
+                       112 "Phase B"))))
+
+  (testing "Custom `school-phases` definitions, with `:ncys` as vector and sequence."
+    (is (= (-> {"Phase A" {:ncys [109 108]}
+                "Phase B" {:ncys '(110 111 112)}}
+               school-phase/ncy->school-phase-map)
+           ;; Note: Map is sorted by NCY
+           (sorted-map 108 "Phase A"
+                       109 "Phase A"
+                       110 "Phase B"
+                       111 "Phase B"
+                       112 "Phase B"))))
+
+  (testing "First school-phase defined containing NCY is used when there are overlaps."
+    (is (= (-> (sorted-map
+                "Phase A" {:ncys [108 109 110]}
+                "Phase B" {:ncys [110 111 112]})
+               keys)
+           ;; Note: Map sorted by keys, so "Phase A" is defined first
+           '("Phase A" "Phase B")))
+    (is (= (-> (sorted-map
+                "Phase A" {:ncys [108 109 110]}
+                "Phase B" {:ncys [110 111 112]})
+               school-phase/ncy->school-phase-map)
+           ;; Note: So school-phase for NCY 110 is "Phase A"
+           (sorted-map 108 "Phase A"
+                       109 "Phase A"
+                       110 "Phase A"
+                       111 "Phase B"
+                       112 "Phase B")))
+    ;; Reversing the definition order
+    (is (= (-> (sorted-map-by #(compare %2 %1)
+                              "Phase A" {:ncys [108 109 110]}
+                              "Phase B" {:ncys [110 111 112]})
+               keys)
+           ;; Note: Map sorted by keys descending, so "Phase B" is defined first
+           '("Phase B" "Phase A")))
+    (is (= (-> (sorted-map-by #(compare %2 %1)
+                              "Phase A" {:ncys [108 109 110]}
+                              "Phase B" {:ncys [110 111 112]})
+               school-phase/ncy->school-phase-map)
+           ;; Note: So school-phase for NCY 110 is "Phase B"
+           (sorted-map 108 "Phase A"
+                       109 "Phase A"
+                       110 "Phase B"
+                       111 "Phase B"
+                       112 "Phase B"))))
+
+  (testing "Repetition of NCY within a definition is ignored."
+    (is (= (-> {"Phase C" {:ncys [120 120 120]}}
+               school-phase/ncy->school-phase-map)
+           {120 "Phase C"}))))
+
 (deftest ncy->school-phase
   (testing "For NCY not in (range -4 (inc 20)), school-phase should be nil."
     (is (->> nil school-phase/ncy->school-phase nil?))
@@ -31,9 +102,7 @@
   (testing "For NCYs 15 to 20 school-phase should be \"post-19\"."
     (is (->> [15 16 17 18 19 20]
              (map school-phase/ncy->school-phase)
-             (every? #{"post-19"}))))
-  )
+             (every? #{"post-19"})))))
 
 (comment
-  (clojure.test/run-tests)
-  )
+  (clojure.test/run-tests))
